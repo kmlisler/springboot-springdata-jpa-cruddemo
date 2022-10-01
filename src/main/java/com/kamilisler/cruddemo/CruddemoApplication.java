fde593cea62b564f03918e8b65d7e8e4b22f4d5f
kmlisler <82888052+kmlisler@users.noreply.github.com>
2022-10-01 15:24:39 +0300

    converting spring data jpa to Spring Data REST for minimize boilerplate code

--- src/main/java/com/kamilisler/cruddemo/CruddemoApplication.java
+++ src/main/java/com/kamilisler/cruddemo/CruddemoApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CruddemoApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(CruddemoApplication.class, args);
+
 
 	}
 
